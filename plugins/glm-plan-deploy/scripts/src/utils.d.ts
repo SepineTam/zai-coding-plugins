00d475443d8955bae088bc30cc4afe76cb974e38
gongchao <chao.gong@aminer.cn>
2025-12-19 14:51:30 +0800

    chore: remove the unpublish plugins

--- plugins/glm-plan-deploy/scripts/src/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const getPackageVersion: () => any;
-export declare const updateDeploySettings: (newSettings: any) => void;
-export declare const loadDeploySettings: () => any;
-export declare function zipFiles(targetFile: string, files: string[]): Promise<File>;
-//# sourceMappingURL=utils.d.ts.map
